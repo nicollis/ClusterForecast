80531d214a7eb067f2b6672eb917bcd52d3bf5e5
Nic Ollis <nic@ollis.me>
2017-06-10 17:24:21 -0400

    implementing bootstrap and creating statis header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.png';
+import {Row, Col, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cities: [
+      {
+        id:  "1.1",
+        name: "Indianapolis, IN"
+      },{
+        id: "1.2",
+        name: "Bloomington, IN"
+      }
+    ],
+      selected_city: "1.1",
+    };
+  }
+
+  cityChanged(city) {
+    this.setState({selected_city: city.id});
+  }
+
+  renderDropdownList() {
+    let cities = this.state.cities;
+    const items = [];
+    for(let i = 0; i < cities.length; i++) {
+      let city = cities[i];
+      items.push(
+        <MenuItem key={city.id} onClick={() => this.cityChanged(city)}>{city.name}</MenuItem>
+      );
+    }
+    return items;
+  }
+
   render() {
     return (
-      <div className="Header">
-        <img src={logo} className="Logo" alt="logo" />
-      </div>
+      <Row className="Header">
+        <Col md={2}>
+          <img src={logo} className="Logo" alt="logo" />
+        </Col>
+        <Col md={2}>
+          <NavDropdown key="1" title={(this.state.cities.find(c => c.id === this.state.selected_city)).name} id="nav-dropdown-within-tab">
+            {this.renderDropdownList()}
+          </NavDropdown>
+        </Col>
+      </Row>
     );
   }
 }
