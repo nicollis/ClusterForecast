5a3f65c04447211f2e5e822e39c1981ed04c418a
Nic Ollis <nic@ollis.me>
2017-06-10 14:41:56 -0400

    added logo and testing heroku deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import logo from '../images/logo.png';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
         <img src={logo} className="Logo" alt="logo" />
-        <h2>Welcome to React</h2>
       </div>
     );
   }
