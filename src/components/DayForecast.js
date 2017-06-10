085e0e600b271848b884288f772eb0ab1d67b6ab
Nic Ollis <nic@ollis.me>
2017-06-10 19:50:34 -0400

    created day forecase component

--- src/components/DayForecast.js
+++ src/components/DayForecast.js
@@ -1,38 +1,69 @@
 import React, { Component } from 'react';
 import { Row, Col, Panel } from 'react-bootstrap';
+import QuickDayStats from './QuickDayStats';
 
 class DayForecast extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quickStats: {
+        yesterday: {
+          text: "Yesterday",
+          score: 'C',
+          conditions: 'Sunny',
+          icon: 'fa-sun-o',
+          chance_of_rain: "0%",
+          modifier: null,
+          next_day_outlook: null,
+        },
+        today: {
+          text: "Today",
+          score: 'B',
+          conditions: 'Sunny',
+          icon: 'fa-sun-o',
+          chance_of_rain: "0%",
+          modifier: "Last Day of The Month",
+          next_day_outlook: "Higher",
+        },
+        tomorrow: {
+          text: "Tomorrow",
+          score: 'A',
+          conditions: 'Storms',
+          icon: 'fa-sun-o',
+          chance_of_rain: "70%",
+          modifier: null,
+          next_day_outlook: null,
+        },
+      }
+    };
+  }
+
   render() {
+    let today = this.state.quickStats.today;
     return (
       <Panel header="TODAY'S FORECAST" className="dayforecast">
         <Row>
           <Col sm={6}>
             <h3 className="title">SCORE</h3>
-            <h1>B</h1>
+            <h1>{today.score}</h1>
           </Col>
           <Col sm={6}>
-            <h3 className="title">Conditions</h3>
-            <Row><i className="fa fa-sun-o fa-5x conditions" aria-hidden="true"></i></Row>
-            <h4 className="info">Last Day of The Month</h4>
+            <h3 className="title">CONDITIONS</h3>
+            <Row><i className={today.icon + " fa fa-5x conditions"} aria-hidden="true"></i></Row>
+            <h4 className="info">{today.modifier}</h4>
           </Col>
         </Row>
         <Row>
           <Col sm={12}>
-            <h3 className="text-left">Tomorrow is forecasted to be <span>HIGHER</span> than today</h3>
+            <h3 className=" info">Tomorrow is forecasted to be <span className="sub-title accent">{today.next_day_outlook.toUpperCase()}</span> than today</h3>
           </Col>
         </Row>
         <Row>
           <Col sm={6}>
-            {/* TOBE MOVED TO A COMPONENT */}
-            <h3 className="text-left">Tomorrow</h3>
-            <h4>Score: B | Conditions: Sunny</h4>
-            <h4>0% chance of rain</h4>
+            <QuickDayStats quickStats={this.state.quickStats.yesterday}/>
           </Col>
           <Col sm={6}>
-            {/* TOBE MOVED TO A COMPONENT */}
-            <h3 className="text-left">Yesterday</h3>
-            <h4>Score: A | Conditions: Raining</h4>
-            <h4>70% chance of rain</h4>
+            <QuickDayStats quickStats={this.state.quickStats.tomorrow}/>
           </Col>
         </Row>
       </Panel>
---
+++ src/components/QuickDayStats.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+class QuickDayStats extends Component {
+  render() {
+    let day = this.props.quickStats;
+    return (
+      <div className="quickdaystats">
+        <h3 className="text-left sub-title">{day.text.toUpperCase()}</h3>
+        <h4 className="info">Score: {day.score} | Conditions: {day.conditions}</h4>
+        <h4 className="info">{day.chance_of_rain} chance of rain</h4>
+      </div>
+    );
+  }
+}
+
+export default QuickDayStats;
