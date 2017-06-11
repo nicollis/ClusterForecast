1f9a36859dcdaf697c79e3bcd0919831343f351b
Nic Ollis <nic@ollis.me>
2017-06-10 20:06:13 -0400

    setting up detailed stats panel

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Header from './Header';
 import DayForecast from './DayForecast';
+import Stats from './Stats'
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
         <Row>
           <Col md={6}>
             <DayForecast/>
+          </Col>
+          <Col md={6}>
+            <Stats/>
           </Col>
         </Row>
       </div>
---
+++ src/components/Stats.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Row, Col, Panel} from 'react-bootstrap';
+
+class Stats extends Component {
+  render() {
+    return (
+      <Panel header="DETAILED STATS" className="stats">
+        
+      </Panel>
+    );
+  }
+}
+
+export default Stats;
